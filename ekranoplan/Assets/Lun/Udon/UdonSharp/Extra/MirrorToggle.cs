b37aef6b5523a803bd6faac90460d681bfd49b7b
손현준 <h991023@naver.com>
2026-09-03 10:40:17 +0900

    Add mirror code
    ミラー表示を切り替えるコードを追加しました。

---
+++ ekranoplan/Assets/Lun/Udon/UdonSharp/Extra/MirrorToggle.cs
@@ -0,0 +1,37 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class MirrorToggle : UdonSharpBehaviour
+{
+    public GameObject MirrorTarget;
+    public Text MirrorTarget_Text;
+    public GameObject MirrorSub;
+    public Text MirrorSub_Text;
+
+    public void ButtonTrigger()
+    {
+        bool isTargetOn = MirrorTarget.activeSelf;
+        bool isSubOn = MirrorSub.activeSelf;
+
+        if (!isTargetOn)
+        {
+            if (isSubOn)
+            {
+                MirrorSub.SetActive(false);
+                MirrorSub_Text.color = new Color(0.5f, 0.5f, 0.5f);
+            }
+            MirrorTarget.SetActive(true);
+            MirrorTarget_Text.color = new Color(1f, 1f, 1f);
+        }
+        else
+        {
+            MirrorTarget.SetActive(false);
+            MirrorTarget_Text.color = new Color(0.5f, 0.5f, 0.5f);
+        }
+
+    }
+}
